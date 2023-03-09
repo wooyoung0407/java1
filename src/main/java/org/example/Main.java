be2fb11462bf172bb4cbe2c5dbb18993515b44e7
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-09 12:43:10 +0900

    java-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,99 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-//        int a = 5;
-//        int b = 10;
+//        int i = 100;
 //
-//        System.out.println("a = " + a);
-//        System.out.println("b = " + b);
-//
-//        System.out.println("============");
-//
-//        int c = a;
-//        a = b;
-//        b = c;
-//
-//        System.out.println("a = " + a);
-//        System.out.println("b = " + b);
+//        while(i > 0){
+//            System.out.println(i);
+//            i--;
+//        }
 
-//        int x = 10;
-//
-//        System.out.println(x + 1);
-//        System.out.println(x - 1);
-//        System.out.println(x * 2);
-//        System.out.println(x / 2);
-//        System.out.println(x % 3);
-
-        int age = 25;
-
-        System.out.println("당신의 나이 : " + age);
-        // 20 <= age <= 34 청년
-        if (age >= 20 && age <= 34) {
-            System.out.println("청년입니다.");
-        } else if (age < 20) {
-            System.out.println("미성년입니다.");
-        } else {
-            System.out.println("어른입니다.");
+        for(int i = 100; i > 0 ; i--){
+            System.out.println(i);
         }
+
+
+
+
+
+
+//        int dan = 8;
+//
+////        int i = 1;
+////
+////        while (i <= 1000) {
+////            System.out.println(dan + " * " + i + " = " + dan * i);
+////            i++;
+////        }
+//
+//        for (int i = 1; i <= 1000; i++) {
+//            System.out.println(dan + " * " + i + " = " + dan * i);
+//        }
+
+
+        // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
+
+
+// 문제 : 할인 대상인지 아닌지 출력해주세요.
+// 조건 : 나이가 19세 이하이거나 60세 이상이면 할인 대상입니다.
+// 조건 : 출력예시 처럼 출력되어야 합니다.
+// 조건 : `구현시작` 부분만 수정 할 수 있습니다.
+// 조건 : 4가지 이상의 방법으로 풀어야 합니다.
+// 조건 : 그 중 2가지 방법은 `&&, ||`없이 풀어야 합니다.
+//        int age = 60; // 이 값을 바꿔가면서 실행해보세요
+//
+//        System.out.println("당신의 나이는 " + age + "살 입니다.");
+//
+//        if (age <= 19) {
+//            System.out.println("할인대상입니다.");
+//        }
+//        if (age >= 60) {
+//            System.out.println("할인대상입니다.");
+//        }
+//        if (age >= 20) {
+//            if (age < 60) {
+//                System.out.println("할인대상이 아닙니다.");
+//            }
+//        }
+
+//        if (age >= 20) {
+//            if (age < 60) {
+//                System.out.println("할인대상이 아닙니다.");
+//            } else if (age <= 19) {
+//                System.out.println("할인대상입니다.");
+//            } else if (age >= 60) {
+//                System.out.println("할인대상입니다.");
+//            }
+//        }
+
+//        if (age <= 19 || age >= 60) {
+//            System.out.println("할인대상입니다.");
+//        } else {
+//            System.out.println("할인대상이 아닙니다.");
+//        }
+//
+//
+//        if (age >= 20 && age < 60) {
+//            System.out.println("할인대상이 아닙니다.");
+//        } else {
+//            System.out.println("할인대상입니다.");
+//        }
+
     }
 }
+
