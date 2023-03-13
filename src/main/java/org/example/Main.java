058827f5c1f2026287e8d48f5dee455a1b241558
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-13 11:21:33 +0900

    java-4.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,51 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
+        검은고양이 a고양이 = new 검은고양이();
+        흰고양이 b고양이 = new 흰고양이();
+        회색고양이 c고양이 = new 회색고양이();
 
-        계산기 a계산기 = new 계산기();
+        a고양이.저주하다();
+        b고양이.치유하다();
+        c고양이.달리다();
 
-        a계산기.합산(3,6);
-        // System.out.println(a계산기.합산(8)); //int 시
     }
 }
 
-class 계산기 {
+class 고양이 {
+    void 달리다() {
+        System.out.println("달리다");
+    }
+    void 멈추다() {
+        System.out.println("멈추다");
+    }
+    void 울다() {
+        System.out.println("울다");
+    }
+    void 물다() {
+        System.out.println("물다");
+    }
+}
 
-    int sum;
+class 검은고양이 extends 고양이 {
+    void 저주하다() {
+        System.out.println("저주하다");
+    }
+}
 
-    void 합산(int n , int m) { // int
-        // int sum = 0;
-        for (int i = n; i <= m; i++) {
-            sum += i;
-        }
-        System.out.println(sum); //return sum;
+class 흰고양이 extends 고양이 {
+    void 치유하다() {
+        System.out.println("치유하다");
+    }
+}
+class 회색고양이 extends  고양이{
+    void 달리다(){
+        //오버라이드
+        System.out.println("회색고양이 달리다.");
     }
 }
