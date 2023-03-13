f09d66793f9f3d9fb7db014a5eefc3e3a0ff7eb3
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-13 12:19:27 +0900

    java-4.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,46 +6,39 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        검은고양이 a고양이 = new 검은고양이();
-        흰고양이 b고양이 = new 흰고양이();
-        회색고양이 c고양이 = new 회색고양이();
 
-        a고양이.저주하다();
-        b고양이.치유하다();
-        c고양이.달리다();
+        검은오리 a검은오리 = new 검은오리();
+        a검은오리.날다();
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
+        고무오리 a고무오리 = new 고무오리();
+        a고무오리.날다();
+        고무2오리 a고무2오리 = new 고무2오리();
+        a고무2오리.날다();
 
     }
 }
 
-class 고양이 {
-    void 달리다() {
-        System.out.println("달리다");
-    }
-    void 멈추다() {
-        System.out.println("멈추다");
-    }
-    void 울다() {
-        System.out.println("울다");
-    }
-    void 물다() {
-        System.out.println("물다");
+class 오리 {
+    void 날다() {
+        System.out.println("날개로 날수있다.");
     }
 }
 
-class 검은고양이 extends 고양이 {
-    void 저주하다() {
-        System.out.println("저주하다");
+class 검은오리 extends 오리 {
+
+}
+
+class 흰오리 extends 오리 {
+
+}
+
+class 고무오리 extends 오리 {
+    void 날다() {
+        System.out.println("고무오리는 날수없다.");
     }
 }
 
-class 흰고양이 extends 고양이 {
-    void 치유하다() {
-        System.out.println("치유하다");
-    }
+class 고무2오리 extends 고무오리 {
 }
-class 회색고양이 extends  고양이{
-    void 달리다(){
-        //오버라이드
-        System.out.println("회색고양이 달리다.");
-    }
-}
+
