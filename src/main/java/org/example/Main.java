e708b100a401e56c1de08c03b3a8db1c50e494bb
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-13 10:47:18 +0900

    java-4.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,38 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         계산기 a계산기 = new 계산기();
-//        void시
-//        a계산기.더하기(4, 3);
-//        a계산기.빼기(5, 3);
-//        a계산기.곱하기(5, 3);
-//        a계산기.나누기(6, 3);
 
-//        int 와 return시
-        System.out.println("더하기 " + a계산기.더하기(4, 3));
-        System.out.println("빼기 " +  a계산기.빼기(5, 3));
-        System.out.println("곱하기 " + a계산기.곱하기(5, 3));
-        System.out.println("나누기 " + a계산기.나누기(6, 3));
+        a계산기.합산(3,6);
+        // System.out.println(a계산기.합산(8)); //int 시
     }
 }
 
 class 계산기 {
-    int 더하기(int a, int b) {
-//        System.out.println("더하기 " + a + "+" + b + "=" + (a + b));
-        return a + b;
-    }
 
-    int 빼기(int a, int b) {
-//        System.out.println("빼기 " + a + "-" + b + "=" + (a - b));
-        return a - b;
-    }
+    int sum;
 
-    int 곱하기(int a, int b) {
-//        System.out.println("곱하기 " + a + "*" + b + "=" + (a * b));
-        return a * b;
-    }
-
-    int 나누기(int a, int b) {
-//        System.out.println("나누기 " + a + "/" + b + "=" + (a / b));
-        return a / b;
+    void 합산(int n , int m) { // int
+        // int sum = 0;
+        for (int i = n; i <= m; i++) {
+            sum += i;
+        }
+        System.out.println(sum); //return sum;
     }
 }
