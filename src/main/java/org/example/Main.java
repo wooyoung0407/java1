fe026db32f8333777c5bd78544dd20587a585e76
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-14 12:10:36 +0900

    java-5.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,28 +11,27 @@ public class Main {
         a전사.공격("브라이언", "칼");
         a전사.재공격();
         a전사.공격("필립", "창");
+        a전사.재공격();
         a전사.공격("마크", "지팡이");
         a전사.재공격();
-        a전사.재공격();
+
     }
 }
 
 class 전사 {
     String name2;
     String weapon2;
-    void 공격(String name, String weapon) {
 
-//        System.out.println(this.name2 + "가(이) " + this.weapon2 + "으(로) 공격합니다."); //매개변수
+    void 공격(String name, String weapon) {
+        System.out.printf("%s이(가)  %s(으)로 공격합니다.\n", name, weapon); //c언어 출력
+        System.out.println(name + "이(가) " + weapon + "(으)로 공격합니다."); // JAVA 출력
 
         this.name2 = name;
         this.weapon2 = weapon;
-        공격출력(this.name2,this.weapon2);
     }
+
     void 재공격() {
-//        System.out.println(this.name2 + "가(이) " + this.weapon2 + "으(로) 공격합니다."); //전역변수(전역변수에 매개변수를 받아와서 출력)
-    }
-    void 공격출력(String name, String weapon){
-            System.out.println(this.name2 + "가(이) " + this.weapon2 + "으(로) 공격합니다.");
+        System.out.printf("%s이(가)  %s(으)로 공격합니다.\n", this.name2, this.weapon2); //c언어 출력
+        System.out.println(this.name2 + "이(가) " + this.weapon2 + "(으)로 공격합니다."); // JAVA 출력
     }
 }
-
