973ba7040e0c63e6835f6efbfef623ee9891d6b8
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-14 13:17:36 +0900

    java-5.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,87 +7,38 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-//        샤오미TV a샤오미TV = new 샤오미TV();
-//        삼성TV a삼성TV = new 삼성TV();
-//        LGTV aLGTV = new LGTV();
+        페라리 a페라리 = new 페라리();
 
-        표준TV a표준TV = new 샤오미TV();
-        a표준TV.켜기();
-        a표준TV.끄기();
+        자동차 a자동차 = (자동차)a페라리;
 
+        a자동차 = a페라리;
 
-        표준TV b표준TV = new 삼성TV();
-        b표준TV.켜기();
-        b표준TV.끄기();
+        페라리 a페라리2 = (페라리)a자동차;
 
-
-        표준TV c표준TV = new LGTV();
-        c표준TV.켜기();
-        c표준TV.끄기();
-
-//
-//        a샤오미TV.켜기();
-//        a샤오미TV.끄기();
-//        a샤오미TV.vr켜기();
-//
-//        a삼성TV.켜기();
-//        a삼성TV.끄기();
-//        a삼성TV.vr켜기();
-//
-//        aLGTV.켜기();
-//        aLGTV.끄기();
-//        aLGTV.게임전환모드();
+        a페라리2.달리다();
+        a페라리2.서다();
+        a페라리2.뚜껑이_열리다();
 
     }
 }
 
-abstract class 표준TV {
-
-    abstract void 켜기();
-
-    abstract void 끄기();
-}
-
-class 샤오미TV extends 표준TV {
-    void 켜기() {
-        System.out.println("샤오미TV가 켜집니다.");
+class 자동차{
+    void 달리다(){
+        System.out.println("달리다");
     }
-
-    void 끄기() {
-        System.out.println("샤오미TV가 꺼집니다.");
-    }
-
-    void vr켜기() {
-        System.out.println("샤오미TV가 vr켜기");
+    void 서다(){
+        System.out.println("서다");
     }
 }
-
-class 삼성TV extends 표준TV {
-    void 켜기() {
-        System.out.println("삼성TV가 켜집니다.");
+class 페라리 extends 자동차{
+    void 뚜껑이_열리다(){
+        System.out.println("뚜껑이 열리다");
     }
-
-    void 끄기() {
-        System.out.println("삼성TV가 꺼집니다.");
+    void 달리다() {
+        System.out.println("달리다2");
     }
-
-    void vr켜기() {
-        System.out.println("삼성TV가 vr켜기");
+    void 서다() {
+        System.out.println("서다2");
     }
 }
-
-class LGTV extends 표준TV {
-    void 켜기() {
-        System.out.println("LGTV가 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("LGTV가 꺼집니다.");
-    }
-
-    void 게임모드전환() {
-        System.out.println("LGTV가 게임모드전환");
-    }
-}
-
 
