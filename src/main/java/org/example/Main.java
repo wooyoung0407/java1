9c9adf42437f3dc84bb3a10e3f635f3a9680c93b
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-15 10:13:08 +0900

    java-6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,38 +7,40 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        페라리 a페라리 = new 페라리();
+        전사 a전사 = new 전사();
+        a전사.이름 = "카니";
+        a전사.나이 =  22;
+        a전사.a무기 = new 칼(); // 칼 a무기 = new 칼();
+        a전사.공격();
 
-        자동차 a자동차 = (자동차)a페라리;
-
-        a자동차 = a페라리;
-
-        페라리 a페라리2 = (페라리)a자동차;
-
-        a페라리2.달리다();
-        a페라리2.서다();
-        a페라리2.뚜껑이_열리다();
+        a전사.이름 = "초코";
+        a전사.a무기 = new 활();
+        a전사.공격();
 
     }
 }
 
-class 자동차{
-    void 달리다(){
-        System.out.println("달리다");
+class 전사 {
+    String 이름;
+    int 나이;
+    무기 a무기;
+    void 공격 (){
+        a무기.공격(this.이름);
     }
-    void 서다(){
-        System.out.println("서다");
+
+}
+abstract class 무기 {
+    abstract void 공격(String 이름);
+}
+class 칼 extends 무기 {
+    void 공격 (String 이름 ){
+        System.out.println(이름 + "가 칼로 공격합니다.");
+
     }
 }
-class 페라리 extends 자동차{
-    void 뚜껑이_열리다(){
-        System.out.println("뚜껑이 열리다");
-    }
-    void 달리다() {
-        System.out.println("달리다2");
-    }
-    void 서다() {
-        System.out.println("서다2");
+class 활 extends 무기 {
+    void 공격 (String 이름){
+        System.out.println(이름 + "가 활로 공격합니다.");
     }
 }
 
