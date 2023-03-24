38e2e3a0ef74b5b0012455523b57a9021157fd54
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-24 10:39:27 +0900

    java-10(비행아이템)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,84 +7,68 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        전사타입A a전사타입A = new 전사타입A();
-        a전사타입A.a무기 = new 칼();
-        a전사타입A.공격();
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
 
-        전사타입B a전사타입B = new 전사타입B();
-        a전사타입B.a무기 = new 활();
-        a전사타입B.공격();
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
 
-        전사타입C a전사타입C = new 전사타입C();
-        a전사타입C.a무기 = new 창();
-        a전사타입C.공격();
+        고무오리 a고무오리 = new 고무오리();
+        a고무오리.날다();
 
-        전사타입D a전사타입D = new 전사타입D();
-        a전사타입D.a무기 = new 도();
-        a전사타입D.공격();
+        고무2오리 a고무2오리 = new 고무2오리();
+        a고무2오리.날다();
     }
 }
-abstract class 전사타입{
-//    String 무기;
-    String 이름;
-    무기 a무기;
-    void 공격(){
-        a무기.공격(this.이름);
-    }
-}
-class 전사타입A extends 전사타입{
-    전사타입A(){
-//        무기 = "칼";
 
-        this.이름 = "전사타입A";
+class 오리 {
+    비행아이템 a비행아이템;
+    오리 (){
+        a비행아이템 = new 비행();
+    }
+    void 날다 (){
+        a비행아이템.날다();
     }
 }
-class 전사타입B extends 전사타입{
-    전사타입B(){
-//        무기 = "창";
-
-        this.이름 = "전사타입B";
+class 청둥오리 extends 오리{
+//    void 날다 (){
+//        System.out.println("오리가 날개로 날아갑니다.");
+//    }
+}
+class 흰오리 extends 오리{
+//    void 날다 (){
+//        System.out.println("오리가 날개로 날아갑니다.");
+//    }
+}
+class 고무오리 extends 오리{
+    고무오리 (){
+        a비행아이템 = new no비행();
+    }
+//    void 날다 (){
+//        System.out.println("저는 날 수 없어요 ㅜㅜ");
+//    }
+}
+class 고무2오리 extends 고무오리{
+    고무2오리 (){
+        a비행아이템 = new no비행();
+    }
+//    void 날다 (){
+//        System.out.println("저는 날 수 없어요 ㅜㅜ");
+//    }
+}
+class 비행아이템 {
+    void 날다 (){
+//        System.out.println("오리가 날개로 날아갑니다.");
     }
 }
-class 전사타입C extends 전사타입{
-    전사타입C(){
-//        무기 = "활";
-
-        this.이름 = "전사타입C";
+class 비행 extends 비행아이템 {
+    void 날다 (){
+        System.out.println("오리가 날개로 날아갑니다.");
     }
 }
-class 전사타입D extends 전사타입{
-    전사타입D(){
-//        무기 = "도";
-
-        this.이름 = "전사타입D";
+class no비행 extends 비행아이템 {
+    void 날다 (){
+        System.out.println("저는 날 수 없어요 ㅜㅜ");
     }
-}
-abstract class 무기 {
-    String weapon;
-    void 공격(String name){
-        System.out.println(name + "가 " + weapon + "로 공격합니다.");
-    }
-}
-class 칼 extends 무기{
-    칼(){
-        weapon = "칼";
-    }
-}
-class 활 extends 무기{
-    활(){
-        weapon = "활";
-    }
-}
-class 창 extends 무기{
-    창(){
-        weapon = "창";
-    }
-
-}
-class 도 extends 무기{
-    도(){
-        weapon = "도";
-    }
-
 }
