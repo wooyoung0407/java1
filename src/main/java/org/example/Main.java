74dcd6d422fb2cb05c8cea3752b672a74e221cca
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-24 10:02:51 +0900

    java-10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,63 +6,85 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        병사 a병사 = new 병사();
-        a병사.이름 = "홍길동";
 
-        a병사.자기소개();
-        // 안녕하세요. 저는 홍길동 이병 입니다.
-        a병사.공격();
+        전사타입A a전사타입A = new 전사타입A();
+        a전사타입A.a무기 = new 칼();
+        a전사타입A.공격();
 
-        a병사.진급();
+        전사타입B a전사타입B = new 전사타입B();
+        a전사타입B.a무기 = new 활();
+        a전사타입B.공격();
 
-        a병사.자기소개();
-        a병사.공격();
-        // 안녕하세요. 저는 홍길동 일병 입니다.
+        전사타입C a전사타입C = new 전사타입C();
+        a전사타입C.a무기 = new 창();
+        a전사타입C.공격();
 
-        a병사.진급();
-
-        a병사.자기소개();
-        a병사.공격();
-
+        전사타입D a전사타입D = new 전사타입D();
+        a전사타입D.a무기 = new 도();
+        a전사타입D.공격();
     }
 }
-class 병사 {
+abstract class 전사타입{
+//    String 무기;
     String 이름;
-    int 계급번호;
-    int 공격력;
-
-    병사() {
-        계급번호 = 1;
-        공격력 = 7;
-    }
-
-    String get계급() {
-
-        String 계급 = "";
-
-        if ( 계급번호 == 1 ) {
-            계급 = "이병";
-        }else if ( 계급번호 == 2 ) {
-            계급 = "일병";
-        }else if ( 계급번호 == 3 ) {
-            계급 = "상병";
-        }else if ( 계급번호 == 4 ) {
-            계급 = "병장";
-        }
-
-        return 계급;
-    }
-
-    void 자기소개() {
-        System.out.println("안녕하세요. 저는 " + this.이름 + this.get계급() + " 입니다.");
-    }
-
-    void 진급() {
-        계급번호++;
-        공격력+=2;
-    }
+    무기 a무기;
     void 공격(){
-        System.out.println(this.이름 + this.get계급() + "이 공격합니다.(공격력 :" + this.공격력 +")");
+        a무기.공격(this.이름);
     }
 }
+class 전사타입A extends 전사타입{
+    전사타입A(){
+//        무기 = "칼";
 
+        this.이름 = "전사타입A";
+    }
+}
+class 전사타입B extends 전사타입{
+    전사타입B(){
+//        무기 = "창";
+
+        this.이름 = "전사타입B";
+    }
+}
+class 전사타입C extends 전사타입{
+    전사타입C(){
+//        무기 = "활";
+
+        this.이름 = "전사타입C";
+    }
+}
+class 전사타입D extends 전사타입{
+    전사타입D(){
+//        무기 = "도";
+
+        this.이름 = "전사타입D";
+    }
+}
+abstract class 무기 {
+    String weapon;
+    void 공격(String name){
+        System.out.println(name + "가 " + weapon + "로 공격합니다.");
+    }
+}
+class 칼 extends 무기{
+    칼(){
+        weapon = "칼";
+    }
+}
+class 활 extends 무기{
+    활(){
+        weapon = "활";
+    }
+}
+class 창 extends 무기{
+    창(){
+        weapon = "창";
+    }
+
+}
+class 도 extends 무기{
+    도(){
+        weapon = "도";
+    }
+
+}
