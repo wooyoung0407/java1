50e64b41a06dac52289c4b54701975186b3c3f24
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-24 11:15:16 +0900

    java-10(오버로딩)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,68 +7,21 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        청둥오리 a청둥오리 = new 청둥오리();
-        a청둥오리.날다();
-        // 출력 : 오리가 날개로 날아갑니다.
+        계산기 a계산기 = new 계산기();
 
-        흰오리 a흰오리 = new 흰오리();
-        a흰오리.날다();
-
-        고무오리 a고무오리 = new 고무오리();
-        a고무오리.날다();
-
-        고무2오리 a고무2오리 = new 고무2오리();
-        a고무2오리.날다();
+        System.out.println(a계산기.더하기(10,20));
+        System.out.println(a계산기.더하기(10,20,30));
+        System.out.println(a계산기.더하기(10.5,20,30));
     }
 }
-
-class 오리 {
-    비행아이템 a비행아이템;
-    오리 (){
-        a비행아이템 = new 비행();
+class 계산기{
+    int 더하기(int x,int y){
+        return x+y;
     }
-    void 날다 (){
-        a비행아이템.날다();
+    int 더하기(int x, int y, int z){
+        return x+y+z;
     }
-}
-class 청둥오리 extends 오리{
-//    void 날다 (){
-//        System.out.println("오리가 날개로 날아갑니다.");
-//    }
-}
-class 흰오리 extends 오리{
-//    void 날다 (){
-//        System.out.println("오리가 날개로 날아갑니다.");
-//    }
-}
-class 고무오리 extends 오리{
-    고무오리 (){
-        a비행아이템 = new no비행();
-    }
-//    void 날다 (){
-//        System.out.println("저는 날 수 없어요 ㅜㅜ");
-//    }
-}
-class 고무2오리 extends 고무오리{
-    고무2오리 (){
-        a비행아이템 = new no비행();
-    }
-//    void 날다 (){
-//        System.out.println("저는 날 수 없어요 ㅜㅜ");
-//    }
-}
-class 비행아이템 {
-    void 날다 (){
-//        System.out.println("오리가 날개로 날아갑니다.");
-    }
-}
-class 비행 extends 비행아이템 {
-    void 날다 (){
-        System.out.println("오리가 날개로 날아갑니다.");
-    }
-}
-class no비행 extends 비행아이템 {
-    void 날다 (){
-        System.out.println("저는 날 수 없어요 ㅜㅜ");
+    double 더하기(double x, double y , double z){
+        return x+y+z;
     }
 }
