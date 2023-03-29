708256a9761e31d96f36cc78a5e6a59c23b27868
깃wooyoung0407 <wooyoung0407@naver.com>
2023-03-29 10:28:19 +0900

    java-12(toString추가)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,21 +18,30 @@ class Solution {
     void run (){
         Person Person1 = new Person("홍길동", 22, 170.5);
         Person Person2 = new Person("이순신", 40, 173.2);
+
+        System.out.println(Person1);
+        System.out.println(Person2);
     }
 }
 
 class Person {
-    String Name;
-    int Age;
-    double Height;
+    private String Name;
+    private int Age;
+    private double Height;
 
     Person(String name, int age, double height) {
         this.Name = name;
         this.Age = age;
         this.Height = height;
 
-        System.out.println("이름 : " + name + " 나이 : " + age + "살 키 : " + height + "cm");
+//        System.out.println("이름 : " + name + " 나이 : " + age + "살 키 : " + height + "cm");
     }
-
+    public String toString() {
+        return "Person{" +
+                "name='" + this.Name + '\'' +
+                ", age=" + this.Age +
+                ", height=" + this.Height +
+                '}';
+    }
 }
 
